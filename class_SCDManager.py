dfa9eefbddaf80f70eeb652565b742eff21ae7b0
edvaldo-gutierres <edvaldo-gutierres>
2024-08-12 20:48:17 +0000

    fix: ajuste class_SCDManager

--- class_SCDManager.py
+++ class_SCDManager.py
@@ -5,6 +5,7 @@ from pyspark.sql.window import Window
 from delta.tables import DeltaTable
 from datetime import datetime
 import pytz
+import logging
 
 class SCDManager:
     def __init__(self):
@@ -130,55 +131,72 @@ class SCDManager:
         matched_columns (list): Lista de colunas usadas para correspondência no merge.
         """
         try:
-            # Determina automaticamente as colunas que devem ser usadas para a geração da chave substituta (SK)
+            # Determina as colunas para a geração da chave substituta (SK)
             columns_to_hash = [col for col in df_origem.columns if col not in ['ROW_INGESTION_TIMESTAMP', 'ROW_VERSION', 'ROW_CURRENT_INDICATOR', 'ROW_EFFECTIVE_DATE', 'ROW_EXPIRATION_DATE', sk_name]]
-            
+
             # Carrega o DataFrame de destino
             df_destino = spark.table(f"{schema_name}.{table_name}")
-            
+
             # Cria uma janela para particionar pelas colunas de particionamento e ordenar por ROW_EFFECTIVE_DATE
             window_spec = Window.partitionBy(*partition_columns).orderBy(col("ROW_EFFECTIVE_DATE").desc())
-            
+
             # Adiciona a coluna row_num para determinar a linha mais recente
-            df_destino = df_destino.withColumn("row_num", row_number().over(window_spec))
-            
+            df_destino = df_destino.withColumn("row_num", row_number().over(window_spec)).filter((col("row_num") == 1) & (col("ROW_CURRENT_INDICATOR") == True)).drop("row_number").select(partition_columns)
+
             # Filtra para manter apenas as linhas mais recentes
-            df_destino = df_destino.filter(col("row_num") == 1).select(*partition_columns).orderBy(partition_columns[0])
-            
+            # df_destino = df_destino.filter(col("row_num") == 1).select(*partition_columns).orderBy(partition_columns[0])
+
             # Realiza o EXCEPT para retornar apenas os registros novos
             df_dados_novos = df_origem.exceptAll(df_destino)
-            
+
             # Gera o timestamp atual para mudança
             change_date = self.generate_change_date("America/Sao_Paulo")
-            
+
             # Adiciona a coluna de chave substituta (SK) e o timestamp de mudança
             df_temp_dados_novos = df_dados_novos.withColumn(
                 sk_name,
                 sha2(concat_ws("|", lit(change_date), *[col(c) for c in columns_to_hash]), 256)
             ).withColumn("CHANGE_DATE", to_timestamp(lit(change_date)))
-            
+
+            # Pega os registros novos com base nas colunas de matched_columns
+            new_data = df_temp_dados_novos.select(matched_columns).distinct()
+
+            # Renomeia a coluna ID no df_temp_dados_novos para evitar ambiguidade
+            df_temp_dados_novos = df_temp_dados_novos.withColumnRenamed(pk_name, f'{pk_name}_novo')
+
             # Pega os dados atuais da dimensão e renomeia as colunas para evitar ambiguidade
             df_atual = (
                 spark.table(f"{schema_name}.{table_name}")
+                .filter(col(pk_name).isin([row[pk_name] for row in new_data.collect()]))
                 .select(
                     col(sk_name).alias(f'{sk_name}_anterior'),
                     *[col(c).alias(f'{c}_anterior') for c in partition_columns],
-                    col("ROW_VERSION").alias("ROW_VERSION_anterior")
+                    col("ROW_VERSION").alias("ROW_VERSION_anterior"),
+                    col(pk_name)
                 )
             )
-            
-            # Junta com df_temp_dados_novos para atualização
+
+            # Junta com df_temp_dados_novos para atualização, usando as colunas renomeadas
             df_temp_dados_novos = df_temp_dados_novos.join(
                 df_atual,
-                on=[col(pk_name) == col(f'{pk_name}_anterior')],
+                on=[df_temp_dados_novos[f'{pk_name}_novo'] == df_atual[f'{pk_name}_anterior']],
                 how="inner"
             )
-            
+
             # Calcula o novo valor de ROW_VERSION
             df_temp_dados_novos = df_temp_dados_novos.withColumn(
                 "ROW_VERSION", coalesce(col("ROW_VERSION_anterior") + 1, lit(1))
             )
-            
+
+            # Cria uma janela para particionar pelos IDs e ordenar por ROW_VERSION em ordem decrescente
+            window_spec = Window.partitionBy(pk_name).orderBy(col("ROW_VERSION_anterior").desc())
+
+            # Adiciona uma coluna de row_number para identificar a linha mais recente
+            df_temp_dados_novos = df_temp_dados_novos.withColumn("row_num", row_number().over(window_spec))
+
+            # Filtra para manter apenas as linhas mais recentes por ID (pk_name)
+            df_temp_dados_novos = df_temp_dados_novos.filter(col("row_num") == 1).drop("row_num")
+
             # Seleciona as colunas finais sem ambiguidade
             df_final = df_temp_dados_novos.select(
                 col(sk_name),
@@ -205,6 +223,9 @@ class SCDManager:
                 }
             ).execute()
             
+            
+            matched_columns.append('ROW_VERSION')
+
             # Segundo MERGE: Inserção de novos registros
             deltaTable.alias("destino").merge(
                 df_final.alias("dados_novos"),
@@ -397,5 +418,3 @@ class SCDManager:
 
         except Exception as e:
             raise Exception(f"Erro ao gerenciar a tabela SCD {schema_name}.{table_name}: {e}")
-        
-
